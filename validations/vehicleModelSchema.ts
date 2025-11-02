ce7613e5afebf55a8c73951d72717537de4c9847
fsa_HaiLH <hailhse183904@fpt.edu.vn>
2025-11-02 14:12:01 +0700

    add validaions

---
+++ validations/vehicleModelSchema.ts
@@ -0,0 +1,12 @@
+import * as Yup from "yup";
+
+export const vehicleModelSchema = Yup.object().shape({
+  name: Yup.string().required("Model name is required"),
+  brand: Yup.string().required("Brand is required"),
+  segment: Yup.string().max(50, "Segment must be less than 50 characters"),
+  description: Yup.string().max(
+    200,
+    "Description must be less than 200 characters"
+  ),
+  active: Yup.boolean(),
+});
